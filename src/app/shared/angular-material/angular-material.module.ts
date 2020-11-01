c027f0866db5738b009563ef029c3481f7af0aeb
sebalr <larrieu.sebastian@gmail.com>
2020-11-01 14:23:13 -0300

    Listing info with hardcoded values

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -4,18 +4,21 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ],
   exports: [
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule
