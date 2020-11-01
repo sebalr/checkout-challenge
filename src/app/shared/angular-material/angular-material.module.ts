dabeb398815069542f991a63054c757b4763bff2
sebalr <larrieu.sebastian@gmail.com>
2020-11-01 12:41:07 -0300

    Reservations review layout
    Start working on reservation layout with hardcoded values.
    Better global class styles

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
+    MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ],
   exports: [
     MatCardModule,
+    MatIconModule,
     MatTabsModule,
     MatToolbarModule
   ]
